b37e55447fb892f8d15104abb81820b04ca120dd
Rafael Carrilho <carrilhorafael57@gmail.com>
2023-07-04 23:59:20 -0700

    bug fixes and started to work with spacex API

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const app = require('./app');
+const server = require('./app');
 const { mongoConnect } = require('./services/mongo');
 const { loadPlanetsData } = require('./models/planets.model');
 
